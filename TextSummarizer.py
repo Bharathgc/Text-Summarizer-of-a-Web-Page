e7ec35e048ece952d8f762106cfd606f102b8104
Bharath Gunari <bharathgc06@gmail.com>
2018-10-10 01:12:13 -0700

    Add files via upload

--- TextSummarizer.py
+++ TextSummarizer.py
@@ -1,32 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[81]:
-
-
 import bs4 as bs
 import urllib.request
 import re
 import nltk
 import heapq
 
-
-# In[82]:
-
-
 #get the data from the paragraph
-page = urllib.request.urlopen("https://en.wikipedia.org/wiki/Artificial_neural_network").read()    
-
-
-# In[83]:
-
+page = urllib.request.urlopen("LINK HERE").read()    
 
 #if you want to see the data in structured format
 print(soup.prettify)
-
-
-# In[84]:
-
 
 paragraphtext = ""
 # i will extract all the data under paragraph <p> tag
@@ -34,10 +18,6 @@ tag = 'p'
 for paragraph in soup.find_all('p'):
     paragraphtext += paragraph.text
 print(paragraphtext)
-
-
-# In[85]:
-
 
 #clean the data
 # remove all the bracketes like [1][2][3] .. etc and replace it with single space
@@ -54,10 +34,6 @@ sentences = nltk.sent_tokenize(text)
 stop_words = nltk.corpus.stopwords.words('english')
 print(sentences)
 
-
-# In[86]:
-
-
 wordlist = [word for word in nltk.word_tokenize(clean_text) if word not in stop_words ]
 for word in wordlist:
     word2count[word] = wordlist.count(word)
@@ -65,10 +41,6 @@ for word in wordlist:
 #calculate the weighted histogram
 for key in word2count.keys():                   
     word2count[key]=word2count[key]/max(word2count.values())
-
-
-# In[87]:
-
 
 # Calculate the score   
 sent2score = {}
@@ -80,10 +52,6 @@ for sentence in sentences:
                      sent2score[sentence]=word2count[word]
                 else:
                     sent2score[sentence]+=word2count[word]
-
-
-# In[88]:
-
 
 best_sentences = heapq.nlargest(7,sent2score,key=sent2score.get)
 for sentences in best_sentences:
